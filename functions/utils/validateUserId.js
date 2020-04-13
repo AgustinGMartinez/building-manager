934ea7cbeee0f5fa112e6a3f7e71a953e02113be
Agustín Martínez <agustin.gervasio.martinez@gmail.com>
2020-04-13 01:00:17 -0300

    babel part 4

--- functions/utils/validateUserId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var validateUserId = function validateUserId(req) {
-  if (req.params.user_id && req.params.user_id !== req.user.id.toString()) throw new CustomError(403, 'Sin trampas please');
-};
-
-exports.validateUserId = validateUserId;
-//# sourceMappingURL=validateUserId.js.map
